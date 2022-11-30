4510aa5cf812e3f8a0086483baeb94f8a321eb24
Marvin <marvin.blachnik004@stud.fh-dortmund.de>
2022-11-30 12:37:15 +0100

    Testphase I Creating file with events & tracks

--- src/ConferenceApplication.java
+++ src/ConferenceApplication.java
@@ -1,5 +1,22 @@
+import controller.IOController;
+import controller.PlanConferenceController;
+import model.ConferenceEvent;
+import model.Track;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+
 public class ConferenceApplication {
-    public static void main(String[] args) {
+    static ArrayList<ConferenceEvent> eventsToSchedule = new ArrayList<>();
+    static ArrayList<Track> tracks = new ArrayList<>();
+
+    public static void main(String[] args) throws ParseException {
+
+        eventsToSchedule = IOController.readKeyboardInput();
+
+        tracks = PlanConferenceController.planConference(eventsToSchedule);
+        IOController.generateOutputFiles(tracks);
+        System.exit(0);
 
     }
 }
--- src/controller/IOController.java
+++ src/controller/IOController.java
@@ -14,47 +14,46 @@ public class IOController {
 
     static ArrayList<ConferenceEvent> eventsToSchedule = new ArrayList<>();
 
-    public static void generateOutputFiles(ArrayList<Track> tracks){
+    public static void generateOutputFiles(ArrayList<Track> tracks) {
         String fileName = new SimpleDateFormat("yyyy-MM-dd  HH-mm'.txt'").format(new Date());
-        try{
+        try {
             FileWriter myWriter = new FileWriter("TRACK-MANAGEMENT_" + fileName);
             for (int i = 0; i < tracks.size(); i++) {
                 ArrayList<String> outputList = tracks.get(i).getOutputList();
                 myWriter.write("Track " + (i + 1) + ":" + System.lineSeparator());
                 System.out.println("");
                 System.out.println("Track " + (i + 1) + ":");
-                for (String str: outputList) {
+                for (String str : outputList) {
 
                     myWriter.write(str + System.lineSeparator());
                     System.out.println(str);
                 }
             }
             myWriter.close();
-            
+
         } catch (IOException e) {
             System.out.println("An error occured.");
             e.printStackTrace();
         }
     }
 
-    public static ArrayList<ConferenceEvent> readKeyboardInput(){
+    public static ArrayList<ConferenceEvent> readKeyboardInput() {
         System.out.println("Please enter conferences: ");
         Scanner keyboard = new Scanner(System.in);
         String line = null;
 
-        try{
-            while(!(line = keyboard.nextLine()).isEmpty()){
+        try {
+            while (!(line = keyboard.nextLine()).isEmpty()) {
                 parseLine(line);
             }
-        }
-        finally {
+        } finally {
             keyboard.close();
         }
         return eventsToSchedule;
     }
 
-    public static void parseLine(String line){
-        try{
+    public static void parseLine(String line) {
+        try {
             String[] words = line.split("\\s+");
             String durationString = words[words.length - 1];
             durationString = durationString.replaceAll("[^\\d]", "");
@@ -64,7 +63,7 @@ public class IOController {
             ConferenceEvent event = new ConferenceEvent(line, length);
             eventsToSchedule.add(event);
             System.out.println(event.toString()); //debug
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println("An error occured.");
             e.printStackTrace();
         }
