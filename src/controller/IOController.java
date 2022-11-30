c643ecc7e1310bcf7356baa74a53b3694f64e593
Marvin <marvin.blachnik004@stud.fh-dortmund.de>
2022-11-30 12:17:42 +0100

    IOController generating output files

--- src/controller/IOController.java
+++ src/controller/IOController.java
@@ -1,13 +1,41 @@
 package controller;
 
 import model.ConferenceEvent;
+import model.Track;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 
 public class IOController {
 
     static ArrayList<ConferenceEvent> eventsToSchedule = new ArrayList<>();
+
+    public static void generateOutputFiles(ArrayList<Track> tracks){
+        String fileName = new SimpleDateFormat("yyyy-MM-dd  HH-mm'.txt'").format(new Date());
+        try{
+            FileWriter myWriter = new FileWriter("TRACK-MANAGEMENT_" + fileName);
+            for (int i = 0; i < tracks.size(); i++) {
+                ArrayList<String> outputList = tracks.get(i).getOutputList();
+                myWriter.write("Track " + (i + 1) + ":" + System.lineSeparator());
+                System.out.println("");
+                System.out.println("Track " + (i + 1) + ":");
+                for (String str: outputList) {
+
+                    myWriter.write(str + System.lineSeparator());
+                    System.out.println(str);
+                }
+            }
+            myWriter.close();
+            
+        } catch (IOException e) {
+            System.out.println("An error occured.");
+            e.printStackTrace();
+        }
+    }
 
     public static ArrayList<ConferenceEvent> readKeyboardInput(){
         System.out.println("Please enter conferences: ");
