e2aa247d34614e0c5f23ec991fcc739f66195466
Marvin <marvin.blachnik004@stud.fh-dortmund.de>
2022-11-30 11:50:48 +0100

    Scheduled events in tracks

--- src/controller/PlanConferenceController.java
+++ src/controller/PlanConferenceController.java
@@ -1,16 +1,48 @@
 package controller;
 
 import model.ConferenceEvent;
+import model.Track;
 
+import java.text.ParseException;
 import java.util.ArrayList;
 
 public class PlanConferenceController {
-    /* implementing the knapsack algorithm into this method
-    the algorithm is from https://www.geeksforgeeks.org/printing-items-01-knapsack/
-	Prints the items which are put in a knapsack of capacity W
-     */
+
+    static ArrayList<Track> resultsTrackList = new ArrayList<Track>();
+
+    public static ArrayList<Track> planConference(ArrayList<ConferenceEvent> eventsToSchedule) throws ParseException{
+        // Knapsack problem
+        while(true){
+            // loop until all events are scheduled in tracks
+            if(eventsToSchedule.isEmpty()){
+                //no more events to schedule
+                break;
+            }
+
+            Track track = new Track();
+            resultsTrackList.add(track);
+
+            //--------------add pre-lunch events--------------
+            printknapSack(180, eventsToSchedule, track.getPreLunchEvents());
+            eventsToSchedule.removeAll(track.getPreLunchEvents());
+            //remove scheduled events from the list
+
+            //---------------add post-lunch events-----------------
+            if(eventsToSchedule.size() != 0){
+                printknapSack(240, eventsToSchedule, track.getPostLunchEvents());
+                eventsToSchedule.removeAll(track.getPostLunchEvents());
+            }
+            track.finalizeTrack();
+        }
+        return resultsTrackList; //output are multiple tracks
+    }
+    
     static void printknapSack(int capacity, ArrayList<ConferenceEvent> sourceList, ArrayList<ConferenceEvent>  targetList)
     {
+        /* implementing the knapsack algorithm into this method
+    the algorithm is from https://www.geeksforgeeks.org/printing-items-01-knapsack/
+	Prints the items which are put in a knapsack of capacity W
+       */
         int n = sourceList.size();
         int i, w;
         int K[][] = new int[n + 1][capacity + 1];
